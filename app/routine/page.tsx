0050bebe4bedc706ec7fa861c543d9c313f62277
Kim Jong IL <jngkim@amazon.com>
2026-02-03 09:39:44 +0900

    feat: reddit

---
+++ app/routine/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import Header from '../components/Header';
+import RoutineGuide from '../components/RoutineGuide';
+
+export default function RoutinePage() {
+  return (
+    <main className="min-h-screen bg-black">
+      <Header />
+      <RoutineGuide />
+    </main>
+  );
+}
